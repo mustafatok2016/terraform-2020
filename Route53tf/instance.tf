0ebb8606ec15a4069b84129f27ba18561b9796b2
mustafatok2016 <mustafa.tk33@gmail.com>
2020-08-14 15:09:01 +0000

    last update

--- Route53tf/instance.tf
+++ Route53tf/instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "task2" {
   ami           = "ami-067f5c3d5a99edc80"
   instance_type = "t2.large"
   associate_public_ip_address = "true"
-  key_name      = "${aws_key_pair.MyMac.key_name}"
+  # key_name      = "${aws_key_pair.MyMac.key_name}"
   user_data     = "${file("userdata.sh")}"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
   availability_zone = "us-west-2a"
@@ -12,7 +12,7 @@ resource "aws_instance" "task2" {
     tags = {
        ami           = "ami-067f5c3d5a99edc80"
   instance_type = "t2.large"
-     
+        Name = "DevOps"
         Team = "DevOps"
         Created_by = "Mustafa"
     }
